7463052c4788288fb6b158d0fcc21e8342a1e4d6
UmitAygunn <umitaygun43@gmail.com>
2023-09-08 12:06:03 +0300

    first commit

---
+++ image/main.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (A) {
+    if (A.target.tagName === "LI") {
+      A.target.classList.toggle("checked");
+      saveData();
+    } else if (A.target.tagName === "SPAN") {
+      A.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
